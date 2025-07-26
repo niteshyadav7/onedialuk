7891de8340aaf238553d2aa929090dea156a2df7
niteshyadav7 <nitesh0709yadav@gmail.com>
2025-07-26 16:05:52 +0530

    basic page layout

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const authRoutes = require("./routes/auth.routes");
+const morgan = require("morgan");
 
 //setup .env
 dotenv.config();
@@ -9,6 +10,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 app.use("/api/auth", authRoutes);
 
 module.exports = app;
